2ddc9923aca960a8f9f4c837f872b3ceaa48add6
0x1cc <0xcanhui@gmail.com>
2023-09-11 03:13:50 -0700

    handle math.ceil&math.floor

--- op-geth/metrics/sample.go
+++ op-geth/metrics/sample.go
@@ -1,12 +1,13 @@
 package metrics
 
 import (
-	cmath "github.com/ethereum/go-ethereum/common/math"
 	"math"
 	"math/rand"
 	"sort"
 	"sync"
 	"time"
+
+	cmath "github.com/ethereum/go-ethereum/common/math"
 )
 
 const rescaleThreshold = time.Hour
@@ -304,7 +305,6 @@ func SamplePercentiles(values int64Slice, ps []float64) []float64 {
 				lower := float64(values[int(pos)-1])
 				upper := float64(values[int(pos)])
 				scores[i] = lower + (pos-cmath.Floor(pos))*(upper-lower)
-				// scores[i] = lower + (pos-float64(int64(pos - 0.5)))*(upper-lower)
 			}
 		}
 	}
