5049bbac511dc91b99e627be9e70a2f39be9ecaf
syntrust <lundeng@quarkchain.org>
2024-12-28 17:36:29 +0800

    wait longer for op-geth verifier

--- op-e2e/system/runcfg/protocol_versions_test.go
+++ op-e2e/system/runcfg/protocol_versions_test.go
@@ -128,9 +128,10 @@ func TestRequiredProtocolVersionChangeAndHalt(t *testing.T) {
 	// Checking if the engine is down is not trivial in op-e2e.
 	// In op-geth we have halting tests covering the Engine API, in op-e2e we instead check if the API stops.
 	_, err = retry.Do(context.Background(), 10, retry.Fixed(time.Second*10), func() (struct{}, error) {
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 		defer cancel()
 		available := client.IsURLAvailable(ctx, sys.NodeEndpoint("verifier").(endpoint.HttpRPC).HttpRPC())
+		t.Log("verifier op-geth is available: ", available)
 		if !available && ctx.Err() == nil { // waiting for client to stop responding to RPC requests (slow dials with timeout don't count)
 			return struct{}{}, nil
 		}
