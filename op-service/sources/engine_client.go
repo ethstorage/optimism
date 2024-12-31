660b65a636df2358dc2d114b1787f87e42a16691
syntrust <lundeng@quarkchain.org>
2024-12-31 18:03:11 +0800

    debug

--- op-service/sources/engine_client.go
+++ op-service/sources/engine_client.go
@@ -159,6 +159,6 @@ func (s *EngineAPIClient) SignalSuperchainV1(ctx context.Context, recommended, r
 		Recommended: recommended,
 		Required:    required,
 	})
-	s.log.Info("engine_signalSuperchainV1", "recommended", recommended, "required", required, "result", result, "err", err)
+	s.log.Warn("engine_signalSuperchainV1", "required", required, "result", result, "err", err)
 	return result, err
 }
