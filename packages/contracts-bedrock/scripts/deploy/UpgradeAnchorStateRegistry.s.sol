9fb471a509073279ea59ed6292dcac90eb0103ff
syntrust <lundeng@quarkchain.org>
2025-01-21 18:59:04 +0800

    fix

--- packages/contracts-bedrock/scripts/deploy/UpgradeAnchorStateRegistry.s.sol
+++ packages/contracts-bedrock/scripts/deploy/UpgradeAnchorStateRegistry.s.sol
@@ -73,13 +73,13 @@ contract UpgradeAnchorStateRegistry is Script {
     ) internal {
         address anchorStateRegistryImpl = DeployUtils.create1({
             _name: "AnchorStateRegistry",
-            _args: DeployUtils.encodeConstructor(abi.encodeCall(IProxy.__constructor__, (_disputeGameFactoryProxy)))
+            _args: DeployUtils.encodeConstructor(abi.encodeCall(IAnchorStateRegistry.__constructor__, (_disputeGameFactoryProxy)))
         });
 
         if (_storageSetter == address(0)) {
             _storageSetter = DeployUtils.create1({
                 _name: "StorageSetter",
-                _args: DeployUtils.encodeConstructor(abi.encodeCall(IProxy.__constructor__, ()))
+                _args: DeployUtils.encodeConstructor("")
             });
         }
 
